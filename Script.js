5621cefd0002a8b29b6e0d0a94bffddd8b7c172b
Gleb <lv.gleb44@gmail.com>
2021-11-03 22:15:04 +0300

    cycles

--- Script.js
+++ Script.js
@@ -1,4 +1,6 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+'use strict';
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", ""); // Написали плюс для того чтобы ответ являлся числом
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,12 +10,25 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?",""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?",""),
-      d = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?",""),
+          b = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != " " && b != " " && a.length < 50) {
+        personalMovieDB.movies[a] = b; // Вместо квадратных скобок можно разместить точку, но тогда вохможны баги
+    } else {
+        i--;
+    };
+};
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довльно мало филмов");
+} else if (personalMovieDB.count >=10 && personalMovieDB.count <= 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман!");
+} else {
+    console.log("Произошла ошибка");
+};
 
 console.log(personalMovieDB);
